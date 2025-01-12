d18ae0794ec07668fdb1117c9dbae8fdc37a31cf
pingfanw <92792652+pingfanw@users.noreply.github.com>
2025-01-12 13:30:15 +0800

    Update network_utils_cifar.py

--- utils/network_utils_cifar.py
+++ utils/network_utils_cifar.py
@@ -1,11 +1,8 @@
 from models.cifar import (alexnet, densenet, resnet,
                           vgg16_bn, vgg19_bn, wrn, preresnet, mlp, pyramidnet)
-# from models.imagenet import (resnext50, resnext101,
-#                               resnext152)
 
 
 def get_network_cifar(network, **kwargs):
-
     networks = {
         'alexnet': alexnet,
         'densenet': densenet,
@@ -17,6 +14,5 @@ def get_network_cifar(network, **kwargs):
         'mlp': mlp,
         'pyramidnet': pyramidnet
     }
-    # print(networks[network])
     return networks[network](**kwargs)
 
